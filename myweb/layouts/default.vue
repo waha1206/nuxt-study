b57a937e93e3abb08693c7118e31067c2248a297
waha1206 <waha1206@gmail.com>
2021-10-01 03:28:39 +0800

    nuxt

---
+++ myweb/layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <header>
+      <h1>這裡是 Header</h1>
+      <a href="/about">關於我</a>
+    </header>
+    <nuxt />
+  </div>
+</template>
---
+++ myweb/pages/about.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>我來自 {{ name }} 端</h1>
+    <nuxt-link to="/">回到首頁</nuxt-link>
+  </div>
+</template>
+<script>
+export default {
+  layout: "default",
+  data({ req }) {
+    return {
+      name: req ? "server" : "client"
+    };
+  }
+};
+</script>
